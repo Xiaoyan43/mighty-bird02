f7110d2f2350ae5f6cd515c3ec3b7352369107bf
naishiama <106795914+naishiama@users.noreply.github.com>
2022-10-25 14:34:47 +1300

    Added updated GUI
    added function to the screenshot button

--- Assets/ScreenShot.cs
+++ Assets/ScreenShot.cs
@@ -6,18 +6,24 @@ using System.IO;
 public class ScreenShot : MonoBehaviour
 {   
 
-    [SerializeField]
-    GameObject blink;
 
-    public void ClickShare() {
-        StartCoroutine(TakeScreenShot());
+
+    public void takescreenShot() {
+        StartCoroutine("screenShot");
     }
 
-    IEnumerator TakeScreenShot() {
-        string fileName = "score.png";
-        string pathToSave = fileName;
-        ScreenCapture.CaptureScreenshot(pathToSave);
+    private IEnumerator screenShot() {
         yield return new WaitForEndOfFrame();
-        Instantiate (blink, new Vector2(0f, 0f), Quaternion.identity);
+        Texture2D screenShot = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+        screenShot.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+        screenShot.Apply();
+        byte[] bytes = screenShot.EncodeToPNG();
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        File.WriteAllBytes(Application.dataPath + "/Screenshot" + timeStamp + ".png", bytes);
+
+        Destroy(screenShot);
+        // string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        // string filePath = Path.Combine( Application.persistentDataPath, "Tanks" + timeStamp + ".png" );
     }
 }
+
